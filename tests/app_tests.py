7123645d5b3ba30b9e285ceb1ed49cbec5f032d7
Arseny <akokotov96@gmail.com>
2024-05-01 11:46:30 -0400

    Tests
    added some unit tests

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -11,16 +11,44 @@ from resources import  *
 
 import unittest
 
-# if __name__ == '__main__':
-#     s = Scrapper()
-#     s.start_driver()
-#     s.login(ID, PASSWORD)
-#     s.go_to('https://twitter.com/Microsoft')
-#     s.get_OP_posts(1)
+class TestScrapper(unittest.TestCase):
+    def test_Soup_functionality(self):
+        posts_OP = get_OP_soup()
+        post_COMMENTER = get_COMMENTER_soup()
+        self.assertTrue(posts_OP) 
+        self.assertTrue(post_COMMENTER) 
+    
+    def test_Table_functionality(self):
+        test_OP_Post_1 = Post("@OPname1", "April 1st", "Joking OP test1", 2, "http://OPtest1.com")
+        test_OP_Post_2 = Post("@OPname2", "April 2nd", "Joking OP test2", 3, "http://OPtest2.com")
 
-# Unit Test 1: Check Soup Functionality
+        test_COMMENTER_Post_1 = Post("@Cname1", "1hr", "OP 1 Commenter test 1", 0, "http://OP1COMtest1.com")
+        test_COMMENTER_Post_2 = Post("@Cname2", "March 1st", "OP 1 Commenter test 2", 1, "http://OP1COMtest2.com")
+        test_COMMENTER_Post_3 = Post("@Cname3", "2hr", "OP 2 Commenter test 1", 3, "http://OP2COMtest1.com")
+        test_COMMENTER_Post_4 = Post("@Cname4", "1min", "OP 2 Commenter test 2", 4, "http://OP2COMtest2.com")
+        test_COMMENTER_Post_5 = Post("@Cname5", "10min", "OP 2 Commenter test 3", 5, "http://OP2COMtest3.com")
 
-# get_OP_soup()
+        OP_POSTS = [test_OP_Post_1, test_OP_Post_2]
+        COMMENTER_POSTS = [[test_COMMENTER_Post_1, test_COMMENTER_Post_2], [test_COMMENTER_Post_3, test_COMMENTER_Post_4, test_COMMENTER_Post_5]]
+
+        s = Scrapper()
+
+        s.df_OP = insert_post_into_Table(s.df_OP, "OP_post1.com",test_OP_Post_1)
+        s.df_OP = insert_post_into_Table(s.df_OP, "OP_post2.com",test_OP_Post_2)
+
+        s.df_COMM = insert_post_into_Table(s.df_COMM, test_OP_Post_1.url,test_COMMENTER_Post_1)
+        s.df_COMM = insert_post_into_Table(s.df_COMM, test_OP_Post_1.url,test_COMMENTER_Post_2)
+        s.df_COMM = insert_post_into_Table(s.df_COMM, test_OP_Post_2.url,test_COMMENTER_Post_3)
+        s.df_COMM = insert_post_into_Table(s.df_COMM, test_OP_Post_2.url,test_COMMENTER_Post_4)
+        s.df_COMM = insert_post_into_Table(s.df_COMM, test_OP_Post_2.url,test_COMMENTER_Post_5)
+
+        self.assertFalse(s.df_OP.empty) 
+        self.assertFalse(s.df_COMM.empty)
+
+        s.clear_data() 
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
